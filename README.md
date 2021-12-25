16d95ffe7e95234620b77d3b3c3ddb1d48fc1fff
jannatabdulghani <jannatabdulghani@gmail.com>
2021-12-25 16:08:08 +0300

    first verrsion

---
+++ README.md
@@ -0,0 +1,1 @@
+# Couza_Store
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#nameOfSalons").click(function() {
+        var open = $(this).data("isopen");
+        if(open) {
+          window.location.href = $(this).val()
+        }
+        //set isopen to opposite so next time when use clicked select box
+        //it wont trigger this event
+        $(this).data("isopen", !open);
+      });
+});
